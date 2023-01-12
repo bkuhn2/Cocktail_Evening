b2dc70afd8ef1173a75e44ebeedea657941ec5f8
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 16:19:14 -0700

    Used useEffect functions so that categories and ingredients state populate on page load

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Home/Home.css'
 import Header from '../Header/Header'
+import fetchCocktailData from '../Utilities/APIcalls'
+import { simplifyCategories, simplifyIngredients, formatSearchResults } from '../Utilities/CleanUp'
 
 const Home = () => {
 
@@ -11,6 +13,17 @@ const Home = () => {
   const [categoryInput, setCategoryInput] = useState('');
   const [nameInput, setNameInput] = useState('');
   const [ingredientInput, setIngredientInput] = useState('');
+
+  useEffect(() => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list`)
+      .then(data => setAllCategories(simplifyCategories(data.drinks)))
+  }, []);
+
+  useEffect(() => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list`)
+      .then(data => setAllIngredients(simplifyIngredients(data.drinks)))
+  }, []);
+
 
   return (
     <div>
