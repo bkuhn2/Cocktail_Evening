2943586df72359410dda4c2c0729e8a41e5cf1ae
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 15:23:57 -0700

    Added state to Home, not set yet

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Home/Home.css'
 import Header from '../Header/Header'
 
 const Home = () => {
+
+  const [allCategories, setAllCategories] = useState([]);
+  const [allIngredients, setAllIngredients] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [ingredientSearchResults, setIngredientSearchResults] = useState([]);
+  const [categoryInput, setCategoryInput] = useState('');
+  const [nameInput, setNameInput] = useState('');
+  const [ingredientInput, setIngredientInput] = useState('');
+
   return (
     <div>
       <Header />
