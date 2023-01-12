347f307ee85400c4ae4dc294bd94bb7571d70204
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 15:10:14 -0700

    Created files  for CocktailDetails component

---
+++ src/Components/CocktailDetails/CocktailDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../CocktailDetails/CocktailDetails.css'
+import Header from '../Header/Header'
+
+const CocktailDetails = () => {
+  return (
+    <div>
+      <Header />
+      <h1>COCKTAIL DETAILS</h1>
+    </div>
+  )
+}
+
+export default CocktailDetails
