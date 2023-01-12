90c49ffa5e4c1be38043df42943358f24c5b516e
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 16:23:46 -0700

    Updated filepath for utility functions

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import '../Home/Home.css'
 import Header from '../Header/Header'
-import fetchCocktailData from '../Utilities/APIcalls'
-import { simplifyCategories, simplifyIngredients, formatSearchResults } from '../Utilities/CleanUp'
+import fetchCocktailData from '../../Utilities/APIcalls'
+import { simplifyCategories, simplifyIngredients, formatSearchResults } from '../../Utilities/CleanUp'
 
 const Home = () => {
 
