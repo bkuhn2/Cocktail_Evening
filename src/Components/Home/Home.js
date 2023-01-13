84c76f604bd8abed5a37d9a2d13f3ec743c4103d
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 18:15:47 -0700

    Wrote functions so that searching for name or ingredient populates searchResults state

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,18 +4,56 @@ import Header from '../Header/Header'
 import SearchForm from '../SearchForm/SearchForm'
 import fetchCocktailData from '../../Utilities/APIcalls'
 import { simplifyCategories, simplifyIngredients, formatSearchResults } from '../../Utilities/CleanUp'
+import { useParams } from 'react-router-dom'
 
 const Home = () => {
 
   const [allIngredients, setAllIngredients] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [ingredientSearchResults, setIngredientSearchResults] = useState([]);
+  const searchTerm = useParams().searchTerm;
+  const ingredientRequest = useParams().ingredientRequest;
+  const ingredientTerm = useParams().ingredientTerm
+
+  const findCocktailsByName = () => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${searchTerm}`)
+      .then(data => {
+        //error handling - make sure we throw error if we get nothing back, i.e. data.drinks === null
+        setSearchResults(formatSearchResults(data.drinks));
+      })
+  }
+
+  const findCocktailsByIngredient = () => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${ingredientTerm}`)
+      .then(data => {
+        setSearchResults(formatSearchResults(data.drinks));
+      })
+  }
 
   useEffect(() => {
     fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list`)
       .then(data => setAllIngredients(simplifyIngredients(data.drinks)))
   }, []);
 
+  useEffect(() => {
+    if (searchTerm) {
+      findCocktailsByName();
+      //error handling, error message reset
+    } else {
+      setSearchResults([])
+      //error handling, error message reset
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (ingredientTerm) {
+      findCocktailsByIngredient();
+      //error handling, error message reset
+    } else {
+      setSearchResults([]);
+      //error handling, error message reset
+    }
+  }, [ingredientTerm])
 
   return (
     <div>
