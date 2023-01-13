9dec418d4aa54aa20e811f43e717fbc19da63d06
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 21:41:55 -0700

    Added functions for selecting a specific ingredient and updated Props being passed down

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import '../Home/Home.css'
 import Header from '../Header/Header'
 import SearchForm from '../SearchForm/SearchForm'
+import IngredientResults from '../IngredientResults/IngredientResults'
+import SearchResultsDisplay from '../SearchResultsDisplay/SearchResultsDisplay'
 import fetchCocktailData from '../../Utilities/APIcalls'
 import { simplifyCategories, simplifyIngredients, formatSearchResults } from '../../Utilities/CleanUp'
 import { useParams } from 'react-router-dom'
@@ -11,24 +13,8 @@ const Home = () => {
   const [allIngredients, setAllIngredients] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [ingredientSearchResults, setIngredientSearchResults] = useState([]);
-  const searchTerm = useParams().searchTerm;
-  const ingredientRequest = useParams().ingredientRequest;
-  const ingredientTerm = useParams().ingredientTerm
-
-  const findCocktailsByName = () => {
-    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${searchTerm}`)
-      .then(data => {
-        //error handling - make sure we throw error if we get nothing back, i.e. data.drinks === null
-        setSearchResults(formatSearchResults(data.drinks));
-      })
-  }
-
-  const findCocktailsByIngredient = () => {
-    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${ingredientTerm}`)
-      .then(data => {
-        setSearchResults(formatSearchResults(data.drinks));
-      })
-  }
+  const [selectedIngredient, setSelectedIngredient] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list`)
@@ -36,37 +22,54 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    if (searchTerm) {
-      findCocktailsByName();
-      //error handling, error message reset
-    } else {
-      setSearchResults([])
-      //error handling, error message reset
-    }
-  }, [searchTerm]);
-
-  useEffect(() => {
-    if (ingredientTerm) {
+    if (selectedIngredient) {
       findCocktailsByIngredient();
       //error handling, error message reset
     } else {
       setSearchResults([]);
       //error handling, error message reset
     }
-  }, [ingredientTerm]);
+  }, [selectedIngredient]);
+  
+  const findCocktailsByName = (input) => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${input}`)
+      .then(data => {
+        //error handling - make sure we throw error if we get nothing back, i.e. data.drinks === null
+        setSearchResults(formatSearchResults(data.drinks));
+      })
+  }
 
-  useEffect(() => {
-    if (ingredientRequest) {
-      findMatchingIngredients();
+  const findCocktailsByIngredient = () => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${selectedIngredient}`)
+      .then(data => {
+        setSearchResults(formatSearchResults(data.drinks));
+      })
+  }
+
+  const selectAnIngredient = (name) => {
+    setSelectedIngredient(name);
+  }
+
+  const searchByName = (input) => {
+    if (input) {
+      findCocktailsByName(input);
+    } else {
+      setSearchResults([]);
+    }
+  }
+
+  const makeNamesList = (input) => {
+    if (input) {
+      findMatchingIngredients(input);
       //error handling, error message reset
     } else {
       setIngredientSearchResults([]);
       //error handling, error message reset
     }
-  }, [ingredientRequest])
+  }
 
-  const findMatchingIngredients = () => {
-    const searchWords = ingredientRequest.toLowerCase().split(' ').filter(word =>  word !== ''); //error handling for things like periods, comma, slashes
+  const findMatchingIngredients = (input) => {
+    const searchWords = input.toLowerCase().split(' ').filter(word =>  word !== ''); //error handling for things like periods, comma, slashes
     console.log(searchWords);
     const ingredientMatches = searchWords.reduce((matchingWords, word) => {
       const matches = allIngredients.filter(ing => ing.toLowerCase().includes(word));
@@ -81,19 +84,31 @@ const Home = () => {
   }
 
   return (
-    <div>
+    <main>
       <Header />
       <h1>Peruse our Plethora of Cocktails</h1>
-      <main className='search-section'>
-        <SearchForm />
+      <section className='search-section'>
+        <div>
+          <SearchForm 
+            searchByName={searchByName} 
+            makeNamesList={makeNamesList} 
+            selectAnIngredient={selectAnIngredient}
+            setSearchResults={setSearchResults}
+          />
+          <IngredientResults 
+            ingredientSearchResults={ingredientSearchResults} 
+            selectAnIngredient={selectAnIngredient}
+          />
+        </div>
         <section className='search-display'>
+          {(!error && searchResults.length !== 0) && <SearchResultsDisplay />}
           {/*conditional if no search results and no error and no ingreds, display text letting user know that results display here */}
           {/* if there's an error, display that
           if there's search results, display SearchResultsDisplay
-          if there's ingredienceResults, display that        */}
+          */}
         </section>
-      </main>
-    </div>
+      </section>
+    </main>
   )
 }
 
