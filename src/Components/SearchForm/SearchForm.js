87e085779cb11c328ab623c4766eb7b5e6389407
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-12 18:14:27 -0700

    Updated inputs to be linked with State and made it so one can't have value while the other does

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../SearchForm/SearchForm.css'
 
@@ -6,21 +6,54 @@ const SearchForm = () => {
 
   const [nameInput, setNameInput] = useState('');
   const [ingredientInput, setIngredientInput] = useState('');
+  const [selectedIngredient, setSelectedIngredient] = useState('');
+
+  useEffect(() => {
+    if (ingredientInput !== '') {
+      setNameInput('');
+    }
+  }, [ingredientInput]);
+
+  useEffect(() => {
+    if (nameInput !== '') {
+      setIngredientInput('');
+    }
+  }, [nameInput]);
+
+  const clearSearchFields = () => {
+    setNameInput('');
+    setIngredientInput('');
+  }
+
 
   return (
     <form className='search-form'>
       <div className='search-type'>
         <p>Search By Cocktail Name</p>
-        <input />
-        <Link to={`/search/${nameInput}`}>
-          <button>Let's Go</button>
+        <input
+          type='text'
+          name='cocktail-name'
+          placeholder='What are you in the mood for?'
+          value={nameInput}
+          onChange={event => setNameInput(event.target.value)}
+        />
+        <Link to={`/search/name/${nameInput}`}>
+          <button onClick={clearSearchFields}>Let's Go</button>
         </Link>
       </div>
       <p>OR</p>
       <div className='search-type'>
         <p>Search By Ingredient</p>
-        <input />
-        <button>Let's Go</button>
+        <input
+          type='text'
+          name='ingredient-name'
+          placeholder='Dig a little deeper...'
+          value={ingredientInput}
+          onChange={event => setIngredientInput(event.target.value)}
+        />
+        <Link to={`/search/ingredientrequest/${ingredientInput}`}>
+          <button onClick={clearSearchFields}>Let's Go</button>
+        </Link>
       </div>
     </form>
   )
