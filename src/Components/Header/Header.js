829b7a788558678b7709d070d534872260138fa7
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-15 11:21:27 -0700

    Added basic styling elements to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../Header/Header.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>HEADER</h1>
-      <Link to='/myevent'>
-        <h2>HERE</h2>
-      </Link>
-      <Link to='/'>
-        <h2>home</h2>
-      </Link>
-    </div>
+    <header>
+      <h1 className='page-title'>Cocktail Evening </h1>
+      <nav className='nav-panel'>
+        <NavLink to='/'>
+          <h2 className='link-text'>Find Cocktails</h2>
+        </NavLink>
+        <NavLink to='/myevent'>
+          <h2 className='link-text'>My Event</h2>
+        </NavLink>
+      </nav>
+    </header>
   )
 }
 
