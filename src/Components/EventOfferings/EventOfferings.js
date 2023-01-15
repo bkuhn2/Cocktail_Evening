8ad3a9f6b7d06fbe02100d4d0245daa1920730ff
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-14 23:35:25 -0700

    Added basic layout and props

--- src/Components/EventOfferings/EventOfferings.js
+++ src/Components/EventOfferings/EventOfferings.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../EventOfferings/EventOfferings.css'
+import EventCocktail from '../EventCocktail/EventCocktail'
 
-const EventOfferings = () => {
+const EventOfferings = ({eventOfferings, removeCocktail}) => {
+  
+  const eventCocktails = eventOfferings.map(cocktail => {
+    return <EventCocktail cocktail={cocktail} removeCocktail={removeCocktail}/>
+  })
+
   return (
-    <div>
-      
+    <div className='event-cocktails-section'>
+      {eventCocktails}
     </div>
   )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Header/Header.css'
 
 const Header = () => {
   return (
     <div>
       <h1>HEADER</h1>
+      <Link to='/myevent'>
+        <h2>HERE</h2>
+      </Link>
+      <Link to='/'>
+        <h2>home</h2>
+      </Link>
     </div>
   )
 }
