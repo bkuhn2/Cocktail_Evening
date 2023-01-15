c229dc839746db308ac9bcaa6ab2cebbc19e39b0
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-14 23:35:12 -0700

    Added delete functionality and basic layout

--- src/Components/EventCocktail/EventCocktail.js
+++ src/Components/EventCocktail/EventCocktail.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import '../EventCocktail/EventCocktail.css'
 
-const EventCocktail = () => {
+const EventCocktail = ({cocktail, removeCocktail}) => {
+
+  const makeIngredientList = () => {
+    if (cocktail.ingredients) {
+      return cocktail.ingredients.map((ing, index) => {
+        return (
+          <li className='ingredient-li' key={index+1}>{ing}</li>
+        )
+      })
+    }
+  }
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    removeCocktail(cocktail.id);
+  }
+
   return (
     <div>
-      
+      <p>{cocktail.name}</p>
+      <img className='event-cocktail-image' src={cocktail.image} alt={`${cocktail.name} image`}/>
+      <ul>
+        {makeIngredientList()}
+      </ul>
+      <button onClick={event => handleDelete(event)}>Remove from my event</button>
     </div>
   )
 }
