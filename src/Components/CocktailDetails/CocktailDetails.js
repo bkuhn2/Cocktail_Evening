ba8bbd0c343abe76f6fe5a7487a0031a28f7a0bb
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-14 19:32:58 -0700

    Added code so that state is set with fetch data upon cocktail detail page load

--- src/Components/CocktailDetails/CocktailDetails.js
+++ src/Components/CocktailDetails/CocktailDetails.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import '../CocktailDetails/CocktailDetails.css'
 import Header from '../Header/Header'
+import fetchCocktailData from '../../Utilities/APIcalls'
+import { formatCocktailData, generateIngredients } from '../../Utilities/CleanUp'
 
 const CocktailDetails = () => {
+
+  const [selectedCocktail, setSelectedCocktail] = useState({});
+  let cocktailID = useParams().id;
+
+  useEffect(() => {
+    fetchCocktailData(`https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${cocktailID}`)
+      .then(data => {
+        setSelectedCocktail(formatCocktailData(data.drinks[0]))
+      })
+  }, [cocktailID])
+
   return (
     <div>
       <Header />
