15e55cfb9c2c8bef7407d5c0dca7f152297aa77f
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-15 21:13:16 -0700

    Refactored to specify between 400 and 500 level errors

--- src/Utilities/APIcalls.js
+++ src/Utilities/APIcalls.js
@@ -3,9 +3,10 @@ const fetchCocktailData = (url) => {
   return fetch(url).then(response => {
     if (response.ok) {
       return response.json()
-    } else {
-      console.log('BAD response: ', response);
+    } else if (response.status >= 500) {
       throw new Error('Failed to load data, our apologies.')
+    } else if (response.status >= 400 && response.status < 500) {
+      throw new Error('400 level error, our apologies - please contact site administrator.')
     }
   })
 }
